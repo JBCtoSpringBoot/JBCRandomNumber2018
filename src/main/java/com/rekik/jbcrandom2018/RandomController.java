f1af80b6d14d88d0bb15cfe554f9fb387d1e7e5f
rekikhaile <rekikhaile@gmail.com>
2018-04-30 14:30:58 -0400

    Random number guessing simple web app

--- src/main/java/com/rekik/jbcrandom2018/RandomController.java
+++ src/main/java/com/rekik/jbcrandom2018/RandomController.java
@@ -1,54 +1,50 @@
 package com.rekik.jbcrandom2018;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.Random;
-import java.util.Scanner;
 
 @Controller
 public class RandomController {
 
+    int randomNumber;
+    Random random = new Random();
+
     @RequestMapping("/")
-    @ResponseBody
-    public String showRandom(){
+    public String addNewRandom(Model model)
+    {
 
-        Scanner scan= new Scanner(System.in);
-        Random random = new Random();
+        return "index";
+    }
 
-        //Commented because it doesn't work
-        /*long from = 1;
-        long to = 100;
-        int randomNumber = random.nextInt(to - from+1) + from;*/
+    @PostMapping("/saverandom")
+    public String saveRandom(@Valid @RequestParam("myRandomGuess") int myRandomGuess, Model model)
+    {
+
+    String higher = "Your guess it too high!";
+    String lower = "Your guess is too low!";
+    String right = "You got it!";
+    String answer = "";
 
         int from = 1;
         int to = 100;
 
-        int randomNumber = random.nextInt(to - from+1) + from;
+        randomNumber = random.nextInt(to - from+1) + from;
 
-        int guessedNumber = 0;
+        model.addAttribute("aRandom",randomNumber);
 
-        System.out.printf("The number is between %d and %d.\n", from, to);
+        if(myRandomGuess > randomNumber)
+            answer = higher;
+        else if(myRandomGuess < randomNumber)
+           answer = lower;
+        else
+            answer = right;
 
-        do{
-            System.out.print("Guess what number is: ");
-            guessedNumber = scan.nextInt();
-           // scan.nextLine();
-
-            System.out.println("The random number is "+ randomNumber);
-            if(guessedNumber > randomNumber)
-                System.out.println("Your guess it too high!");
-            else if(guessedNumber < randomNumber)
-                System.out.println("Your guess is too low!");
-            else System.out.println("You got it!");
-        }while (guessedNumber!=randomNumber);
-
-        String randomStr = String.valueOf(randomNumber);
-
-        //After the program is done you can see the random number on your browser http://localhost:8080/
-        return "The random number is "+randomStr;
-
+        model.addAttribute("answer",answer);
+        return "list";
     }
 
 
